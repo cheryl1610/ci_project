02ce3ac49152c79e5a634e8940d0234492705fbb
Cheryl Gilham <cheryl.thursby@gmail.com>
2025-04-25 10:02:16 +0100

    fixed break

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world?"
+    return "Hello, world!"
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
